8a15b3ee395b5cf778e0354ae85609bae219fada
lubase <10523320+small_garden@user.noreply.gitee.com>
2024-06-06 19:51:20 +0800

    去掉对dm_form_backfill表的使用

--- lubase-core/src/main/java/com/lubase/core/model/customForm/FormRule.java
+++ lubase-core/src/main/java/com/lubase/core/model/customForm/FormRule.java
@@ -15,9 +15,4 @@ public class FormRule {
      * 表单字段级联过滤规则
      */
     private List<FieldRelyModel> fieldRely;
-
-    /**
-     * 表单字段回填规则
-     */
-    private List<FieldBackfillModel> fieldBackfill;
 }
