410448b80d8aad03ebf783b069d36b1c89680383
lubase <10523320+small_garden@user.noreply.gitee.com>
2024-06-07 22:22:27 +0800

    表单子表删除按钮功能实现

--- lubase-core/src/main/java/com/lubase/core/service/button/SpecialButtonService.java
+++ lubase-core/src/main/java/com/lubase/core/service/button/SpecialButtonService.java
@@ -5,10 +5,13 @@ import com.lubase.core.entity.DmCustomFormEntity;
 import com.lubase.core.model.customForm.ChildTableSetting;
 import com.lubase.core.service.CustomFormDataService;
 import com.lubase.orm.exception.InvokeCommonException;
+import com.lubase.orm.exception.WarnCommonException;
 import com.lubase.orm.service.DataAccess;
 import com.lubase.orm.util.TypeConverterUtils;
 import com.lubase.core.entity.SsButtonEntity;
 import com.lubase.core.model.ButtonServerSettingModel;
+import lombok.SneakyThrows;
+import org.awaitility.constraint.WaitConstraint;
 import org.springframework.util.StringUtils;
 
 /**
@@ -22,6 +25,7 @@ public interface SpecialButtonService {
      * @return
      * @throws InvokeCommonException
      */
+    @SneakyThrows
     default ButtonServerSettingModel getMainTableCode(CustomFormDataService customFormDataService, SsButtonEntity button) throws InvokeCommonException {
         // 通过是否有serial_num 来识别是子表按钮
         Boolean isFormButton = button.containsKey("serial_num");
@@ -31,8 +35,12 @@ public interface SpecialButtonService {
             if (formEntity == null) {
                 throw new InvokeCommonException("根据按钮无法找到归属表单，请联系管理员");
             }
+            ChildTableSetting childTable = customFormDataService.getChildTableFromSettingStr(formEntity.getChild_table(), button.get("serial_num").toString());
             // 子表删除按钮暂时只支持物理删除
-            String mainTableCode = formEntity.getTable_code();
+            if (childTable == null || childTable.getQueryOption() == null || StringUtils.isEmpty(childTable.getQueryOption().getTableName())) {
+                throw new WarnCommonException("子表数据源为配置TableName属性");
+            }
+            String mainTableCode = childTable.getQueryOption().getTableName();
             ButtonServerSettingModel serverSettingModel = new ButtonServerSettingModel();
             serverSettingModel.setMainTableCode(mainTableCode);
             serverSettingModel.setIsLogicDelete(false);
