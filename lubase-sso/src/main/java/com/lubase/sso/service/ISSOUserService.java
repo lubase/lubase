5fc282cd43fbd967fb18772f3e4bf88bee697699
bluesky <blue_sky_py@163.com>
2024-06-30 22:16:32 +0800

    修改方法保护范围

--- lubase-sso/src/main/java/com/lubase/sso/service/ISSOUserService.java
+++
@@ -1,9 +0,0 @@
-package com.lubase.sso.service;
-
-
-import com.lubase.core.aop.LoginInfoVO;
-import com.lubase.orm.model.LoginUser;
-
-public interface ISSOUserService {
-    LoginUser getSSOUser(LoginInfoVO loginInfo);
-}
