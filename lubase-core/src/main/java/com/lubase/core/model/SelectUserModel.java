a601287bfc32f68d4402634fb92744a84da50803
LuBase <10523320+small_garden@user.noreply.gitee.com>
2024-09-06 21:42:04 +0800

    优化系统服务列显示名称

--- lubase-core/src/main/java/com/lubase/core/model/SelectUserModel.java
+++ lubase-core/src/main/java/com/lubase/core/model/SelectUserModel.java
@@ -1,5 +1,6 @@
 package com.lubase.core.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 /**
@@ -8,8 +9,12 @@ import lombok.Data;
 @Data
 public class SelectUserModel {
     private String id;
+    //设置json序列化后别名为user_code
+    @JsonProperty("user_code")
     private String userCode;
+    @JsonProperty("user_name")
     private String userName;
+    @JsonProperty("organization_id")
     private String deptId;
     private String parentDeptId;
     private String deptName;
